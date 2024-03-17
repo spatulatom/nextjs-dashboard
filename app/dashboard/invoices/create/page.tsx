4a70213dfe21bf21f0df485e5a8a4fc54df34d6f
spatulatom <spatulatom@gmail.com>
2024-03-17 21:57:09 +0000

    no form for now

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const customers = await fetchCustomers();
+  // const customers = await fetchCustomers();
 
   return (
     <main>
@@ -22,7 +22,7 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} />
+      {/* <Form customers={customers} /> */}
     </main>
   );
 }
